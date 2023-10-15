f0b374da687561b297ea419f23e0a8787f2d97d4
viniciusfonseca <vfonseca1618@gmail.com>
2023-10-15 13:36:24 -0300

    add build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 (cd rinha-json; wasm-pack build --target no-modules)
-(cd json-viewer; npm i; npm run build)
+(cd json-viewer; bun i; bun run build)
+
+rm -rf dist
+mkdir dist
+cp ./{index.html,index.js,parser.js,spinner.gif} dist
+mkdir -p dist/json-viewer/dist
+cp json-viewer/dist/* dist/json-viewer/dist
+mkdir -p dist/rinha-json/pkg
+cp rinha-json/pkg/* dist/rinha-json/pkg
