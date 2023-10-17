70c260aeeb1f9b051cef37ba84f80e4cf68a6902
viniciusfonseca <vfonseca1618@gmail.com>
2023-10-16 22:45:46 -0300

    remove wasm

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-(cd rinha-json; wasm-pack build --target no-modules)
 (cd json-viewer; bun i; bun run build)
 
 rm -rf dist
@@ -8,5 +7,3 @@ mkdir dist
 cp ./{index.html,index.js,parser.js,spinner.gif} dist
 mkdir -p dist/json-viewer/dist
 cp json-viewer/dist/* dist/json-viewer/dist
-mkdir -p dist/rinha-json/pkg
-cp rinha-json/pkg/* dist/rinha-json/pkg
